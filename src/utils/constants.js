ef6fcd090cce616855d9be3ecb5116f42723b9d7
Derryck <oseiagyemanb@gmail.com>
2023-08-31 17:32:16 +0100

    update of constants file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi';
+import { BsYoutube, BsFacebook, BsTwitter, BsInstagram } from 'react-icons/bs';
+import { FaTiktok } from 'react-icons/fa';
+
 export const links = [
 	{
 		id: 1,
@@ -23,19 +26,110 @@ export const services = [
 		id: 1,
 		icon: <GiCompass />,
 		title: 'mission',
-		text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+		text: '"Elevating style with a curated collection. Empowering individuality. Embracing sustainability. Redefining fashion, one conscious choice at a time."',
 	},
 	{
 		id: 2,
 		icon: <GiDiamondHard />,
 		title: 'vision',
-		text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+		text: 'To be the global destination for fashion enthusiasts, uniting diverse styles with ethical choices, and inspiring confident self-expression.',
 	},
 	{
 		id: 3,
 		icon: <GiStabbedNote />,
 		title: 'history',
-		text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+		text: 'Founded with a passion for style in 2020. Evolved into a premier online fashion hub, blending trends, quality, and accessibility.',
+	},
+];
+
+export const customer_service = [
+	{
+		id: 1,
+		text: 'contact us',
+	},
+	{
+		id: 2,
+		text: 'track an order',
+	},
+	{
+		id: 3,
+		text: 'delivery',
+	},
+	{
+		id: 4,
+		text: 'exhange and return policy',
+	},
+	{
+		id: 5,
+		text: 'delivery',
+	},
+	{
+		id: 6,
+		text: 'create returns',
+	},
+	{
+		id: 7,
+		text: 'gifts cards',
+	},
+	{
+		id: 8,
+		text: 'FAQ',
+	},
+];
+export const about_fashion_dial = [
+	{
+		id: 1,
+		text: 'our story',
+	},
+	{
+		id: 2,
+		text: 'advertising',
+	},
+	{
+		id: 3,
+		text: 'affiliates',
+	},
+	{
+		id: 4,
+		text: 'careers',
+	},
+	{
+		id: 5,
+		text: 'partner boutique',
+	},
+	{
+		id: 6,
+		text: 'corporate social responsibility',
+	},
+	{
+		id: 7,
+		text: 'get email update',
+	},
+	{
+		id: 8,
+		text: 'fashion dial blog',
+	},
+];
+export const social_icons = [
+	{
+		id: 1,
+		icon: <BsYoutube />,
+	},
+	{
+		id: 2,
+		icon: <BsFacebook />,
+	},
+	{
+		id: 3,
+		icon: <BsInstagram />,
+	},
+	{
+		id: 4,
+		icon: <BsTwitter />,
+	},
+	{
+		id: 5,
+		icon: <FaTiktok />,
 	},
 ];
 
